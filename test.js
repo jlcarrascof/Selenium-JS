34be1915c98ff9db1be8dcd353a4ad71f822d5a8
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-04-27 21:39:39 -0400

    Add initial test script using selenium-webdriver for browser automation

---
+++ test.js
@@ -0,0 +1,12 @@
+const { Builder, By, until } = require('selenium-webdriver');
+
+(async function example() {
+  let driver = await new Builder().forBrowser('firefox').build();
+  try {
+    await driver.get('http://www.google.com');
+    await driver.findElement(By.name('q')).sendKeys('Selenium', Key.RETURN);
+    await driver.wait(until.titleContains('Selenium'), 1000);
+  } finally {
+    await driver.quit();
+  }
+})();
