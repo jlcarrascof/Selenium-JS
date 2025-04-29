b2b327efa05f33da71ee3661f05e1bb65e187beb
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-04-29 08:35:06 -0400

    Add navigation to application in example function in firstTest.js

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -5,9 +5,9 @@ async function example() {
 // Launch the browser
 let driver = await new Builder().forBrowser('firefox').build();
 
-
-
 // Navigate to our application
+
+await driver.get('http://lambdatest.github.io/sample-todo-app/');
 
 // Add a todo
 
