6804f63e3b3b0cc709bd4c705476c371d129fe1f
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-04-29 11:41:52 -0400

    Add initial harmonyTest.js file with browser launch and navigation

---
+++ tests/harmonyTest.js
@@ -0,0 +1,20 @@
+const { Builder, By, Key } = require('selenium-webdriver');
+
+async function example() {
+
+// Launch the browser
+let driver = await new Builder().forBrowser('firefox').build();
+
+// Navigate to our application
+
+await driver.get('https://qa.harmonychurchsuite.com/tenant/groups/index');
+
+// Add a todo
+//await driver.findElement(By.id('sampletodotext')).sendKeys('Learn //Selenium', Key.RETURN);
+
+// Close the browser
+await driver.quit();
+
+}
+
+example();
