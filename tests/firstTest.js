63528e8d41154971dfaed9af93b0cbdaba42814a
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-04-29 08:31:44 -0400

    Add driver initialization in example function in firstTest.js

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -3,6 +3,9 @@ const { Builder } = require('selenium-webdriver');
 async function example() {
 
 // Launch the browser
+let driver = await new Builder().forBrowser('firefox').build();
+
+
 
 // Navigate to our application
 
